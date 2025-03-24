b9462d590d99b74bc004e3c8f32179ba5d0f1f26
domparsons <puffins_fridge0d@icloud.com>
2025-03-24 23:18:16 +0000

    fix: settings

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -6,8 +6,8 @@ class Settings(BaseSettings):
     PROJECT_VERSION: str = "1.0.0"
     DATABASE_URL: str = "sqlite:///./test.db"
     API_V1_STR: str = "/api/v1"
-    secret_key: str
-    algorithm: str
+    secret_key: str = ""
+    algorithm: str = ""
 
     class Config:
         env_file = ".env"
