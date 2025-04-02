4e0b0f902e986775123034fdaf2faef728353a49
domparsons <puffins_fridge0d@icloud.com>
2025-04-02 20:47:20 +0100

    vercel routing

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,12 +1,16 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+  server: {
+    historyApiFallback: true,
+  },
+})
