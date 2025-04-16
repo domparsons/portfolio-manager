434940899393af5625defe345b6f03ef8f03dab6
domparsons <puffins_fridge0d@icloud.com>
2025-04-16 23:15:26 +0100

    cleanup

--- dashboard/src/app/assets/asset-table.tsx
+++ dashboard/src/app/assets/asset-table.tsx
@@ -7,13 +7,6 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip'
-import { Info, SquareArrowOutUpRight } from 'lucide-react'
 import { SheetTrigger } from '@/components/ui/sheet'
 interface Asset {
   id: number
