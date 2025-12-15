ad690fd4869c4894b23df3535d44995c7df24b4d
domparsons <puffins_fridge0d@icloud.com>
2025-12-15 13:28:26 +0000

    feat: asset loading

--- dashboard/src/app/table-skeleton.tsx
+++ dashboard/src/app/table-skeleton.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const TableSkeleton = () => {
   return (
-    <div className="space-y-4">
-      {Array.from({ length: 25 }).map((_, index) => <Skeleton key={index} className="h-[28px] rounded-full" />)}
+    <div className="space-y-4 mt-4">
+      {Array.from({ length: 25 }).map((_, index) => (
+        <Skeleton key={index} className="h-[28px] rounded-full" />
+      ))}
     </div>
   );
 };
