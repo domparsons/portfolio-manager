693ba1ed3c7013c36f83fda7f6369598b70554a8
domparsons <puffins_fridge0d@icloud.com>
2026-01-11 16:38:30 +0000

    feat: update sidebar

---
+++ dashboard/src/app/backtesting/backtest-history.tsx
@@ -0,0 +1,10 @@
+const BacktestHistory = () => {
+  return (
+    <div className="dashboard">
+      <h1 className="text-2xl font-semibold">Backtest History</h1>
+      <p className={"mt-4"}>Backtest history coming soon</p>
+    </div>
+  );
+};
+
+export { BacktestHistory };
